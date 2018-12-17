66aa5c181d92f892d9d334ffdb76996a5072017c
Krendelev <krendelev@gmail.com>
2018-12-18 01:59:55 +0300

    add bot layout

--- castress/bot.py
+++ castress/bot.py
@@ -1,2 +1,22 @@
-from telegram.ext import CommandHandler, Filters, MessageHandler, Updater
-from config import PROXY, TOKEN
+def start():
+    pass
+
+
+def save_user():
+    pass
+
+
+def check_last_visit():
+    pass
+
+
+def list_unheard():
+    pass
+
+
+def get_podcast():
+    pass
+
+
+def get_more():
+    pass
