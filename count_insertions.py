099a7028e3a63352bab54016ef774c8f87b0e026
AlexeyDoroshenko <spartacity@mail.ru>
2018-12-22 16:51:13 +0300

    Implement count_insertions

---
+++ count_insertions.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+
+html = open("test.html", "r", encoding="utf-8")
+
+
+def count_insertions(html):
+    """
+    Посчитать количество вставок (код, картинки) в статье
+    Аргумент: объект BeautifulSoup
+    """
+    soup = BeautifulSoup(html, "html.parser")
+    article_soup = soup.find(
+        "div", class_="post__text post__text-html js-mediator-article"
+    )
+    article_image = article_soup.find_all("img")
+    count_article_image = len(article_image)
+    return count_article_image
+
+
+if __name__ == "__main__":
+    count_insertions(html)
