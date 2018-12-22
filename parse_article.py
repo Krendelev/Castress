70f2e038f6bf22fa9fdd4e0d92522663a01c2a02
AlexeyDoroshenko <spartacity@mail.ru>
2018-12-23 00:41:05 +0300

    Ready text_processing

--- parse_article.py
+++ parse_article.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
 
-html = open("test.html", "r", encoding="utf-8")
+with open("test.html", "r", encoding="utf-8") as test:
+    html = test.read()
+
 SYMBOL_LIMIT = 2000
 accents = {"джуниор": "дж+униор", "Джуниор": "Дж+униор"}
 
@@ -11,19 +13,22 @@ def parse_article(html):
     Аргумент: объект BeautifulSoup
     Результат: текст статьи очищенный от тегов
     """
-    soup_text = BeautifulSoup(html, "html.parser")
-    # заголовок
-    header_with_tags = soup_text.find("title")
-    header = header_with_tags.get_text()
-    # текст
-    text_with_tags = soup_text.find(
-        "div", class_="post__text post__text-html js-mediator-article"
-    )
-    text = text_with_tags.get_text()
-    # все вместе
-    all_text = ("{0}\n{1}").format(header, text)
-    all_text = all_text.replace("\n", " ")
-    return all_text
+    try:
+        soup_text = BeautifulSoup(html, "html.parser")
+        # заголовок
+        header_with_tags = soup_text.find("title")
+        header = header_with_tags.get_text()
+        # текст
+        text_with_tags = soup_text.find(
+            "div", class_="post__text post__text-html js-mediator-article"
+        )
+        text = text_with_tags.get_text()
+        # все вместе
+        all_text = ("{0}\n{1}").format(header, text)
+        all_text = all_text.replace("\n", " ")
+        return all_text
+    except:
+        return None
 
 
 def count_insertions(html):
@@ -32,16 +37,34 @@ def count_insertions(html):
     Аргумент: объект BeautifulSoup
     Результат: количество картинок в статье
     """
-    soup = BeautifulSoup(html, "html.parser")
-    article_soup = soup.find(
-        "div", class_="post__text post__text-html js-mediator-article"
-    )
     try:
-        article_image = article_soup.find_all("img")
-        count_article_image = len(article_image)
-    except AttributeError:
-        count_article_image = 0
-    return count_article_image
+        soup = BeautifulSoup(html, "html.parser")
+        article_soup = soup.find(
+            "div", class_="post__text post__text-html js-mediator-article"
+        )
+        try:
+            article_image = article_soup.find_all("img")
+            count_article_image = len(article_image)
+        except AttributeError:
+            count_article_image = 0
+        return count_article_image
+    except:
+        return None
+
+
+def saved_synopses(html):
+    """
+    Сохранить синопсисы
+    Аргумент: объект BeautifulSoup
+    Результат: текст синопсиса
+    """
+    try:
+        soup = BeautifulSoup(html, "html.parser")
+        soup_synopsis = soup.find("meta", {"name": "description"})
+        synopsis = soup_synopsis.get("content")
+        return synopsis
+    except:
+        return None
 
 
 def place_accents(all_text, accents):
@@ -62,17 +85,21 @@ def get_text_chunk(all_text):
     Аргумент: текст, размер
     Результат: синтаксически полный фрагмент текста
     """
-    all_text = all_text.split(". ")
-    text_cake = []
-    text_cake_len = 0
-    for text_piece in all_text:
-        if not text_cake_len + len(text_piece) > SYMBOL_LIMIT:
-            text_cake_len += len(text_piece)
-            text_cake.append(text_piece)
-    separator = ". "
-    return separator.join(text_cake)
+    try:
+        all_text = all_text.split(". ")
+        text_cake = []
+        text_cake_len = 0
+        for text_piece in all_text:
+            if not text_cake_len + len(text_piece) > SYMBOL_LIMIT:
+                text_cake_len += len(text_piece)
+                text_cake.append(text_piece)
+        separator = ". "
+        return separator.join(text_cake)
+    except:
+        return None
 
 
 if __name__ == "__main__":
     get_text_chunk(place_accents(parse_article(html), accents))
     count_insertions(html)
+    saved_synopses(html)
