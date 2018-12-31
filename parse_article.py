228398c981a820ea7b184383c2d8feea80895aca
AlexeyDoroshenko <spartacity@mail.ru>
2018-12-31 12:02:20 +0300

    Corrected except settings

--- parse_article.py
+++ parse_article.py
@@ -10,81 +10,72 @@ accents = {"джуниор": "дж+униор", "Джуниор": "Дж+унио
 def parse_article(html):
     """
     Извлечь текст
-    Аргумент: объект BeautifulSoup
+    Аргумент: строка с html
     Результат: текст статьи очищенный от тегов
     """
-    try:
-        soup_text = BeautifulSoup(html, "html.parser")
-        # заголовок
-        header_with_tags = soup_text.find("title")
-        header = header_with_tags.get_text()
-        # текст
-        text_with_tags = soup_text.find(
-            "div", class_="post__text post__text-html js-mediator-article"
-        )
-        text = text_with_tags.get_text()
-        # все вместе
-        all_text = ("{0}\n{1}").format(header, text)
-        all_text = all_text.replace("\n", " ")
-        return all_text
-    except:
-        return None
+    soup_text = BeautifulSoup(html, "html.parser")
+    # заголовок
+    header_with_tags = soup_text.find("title")
+    header = header_with_tags.get_text()
+    # текст
+    text_with_tags = soup_text.find(
+        "div", class_="post__text post__text-html js-mediator-article"
+    )
+    text = text_with_tags.get_text()
+    # все вместе
+    all_text = ("{0}. {1}").format(header, text)
+    all_text = all_text.replace("\n", " ")
+    return all_text
 
 
 def count_insertions(html):
     """
     Посчитать количество вставок (код, картинки) в статье
-    Аргумент: объект BeautifulSoup
+    Аргумент: строка с html
     Результат: количество картинок в статье
     """
-    try:
-        soup = BeautifulSoup(html, "html.parser")
-        article_soup = soup.find(
-            "div", class_="post__text post__text-html js-mediator-article"
-        )
-        try:
-            article_image = article_soup.find_all("img")
-            count_article_image = len(article_image)
-        except AttributeError:
-            count_article_image = 0
-        return count_article_image
-    except:
-        return None
+    soup = BeautifulSoup(html, "html.parser")
+    article_soup = soup.find(
+        "div", class_="post__text post__text-html js-mediator-article"
+    )
+    article_image = article_soup.find_all("img")
+    if article_image:
+        count_article_image = len(article_image)
+    else:
+        count_article_image = 0
+    return count_article_image
 
 
 def the_presence_of_code(html):
     """
     Проверить статью на наличие кода
-    Аргумент: объект BeautifulSoup
+    Аргумент: строка с html
     Результат: информация о наличии кода в статье
     """
-    try:
-        soup = BeautifulSoup(html, "html.parser")
-        tag_code_soup = soup.find(
-            "div", class_="post__text post__text-html js-mediator-article"
-        )
-        try:
-            code = tag_code_soup.find("code")
-            code = code.get_text()
-            return True
-        except AttributeError:
-            return False
-    except:
-        return None
+
+    soup = BeautifulSoup(html, "html.parser")
+    tag_code_soup = soup.find(
+        "div", class_="post__text post__text-html js-mediator-article"
+    )
+    code = tag_code_soup.find("code")
+    if code:
+        return True
+    else:
+        return False
 
 
 def saved_synopses(html):
     """
     Сохранить синопсисы
-    Аргумент: объект BeautifulSoup
+    Аргумент: строка с html
     Результат: текст синопсиса
     """
-    try:
-        soup = BeautifulSoup(html, "html.parser")
-        soup_synopsis = soup.find("meta", {"name": "description"})
+    soup = BeautifulSoup(html, "html.parser")
+    soup_synopsis = soup.find("meta", {"name": "description"})
+    if soup_synopsis:
         synopsis = soup_synopsis.get("content")
         return synopsis
-    except:
+    else:
         return None
 
 
@@ -106,18 +97,15 @@ def get_text_chunk(all_text):
     Аргумент: текст, размер
     Результат: синтаксически полный фрагмент текста
     """
-    try:
-        all_text = all_text.split(". ")
-        text_cake = []
-        text_cake_len = 0
-        for text_piece in all_text:
-            if not text_cake_len + len(text_piece) > SYMBOL_LIMIT:
-                text_cake_len += len(text_piece)
-                text_cake.append(text_piece)
-        separator = ". "
-        return separator.join(text_cake)
-    except:
-        return None
+    all_text = all_text.split(". ")
+    text_cake = []
+    text_cake_len = 0
+    for text_piece in all_text:
+        if not text_cake_len + len(text_piece) > SYMBOL_LIMIT:
+            text_cake_len += len(text_piece)
+            text_cake.append(text_piece)
+    separator = ". "
+    return separator.join(text_cake)
 
 
 if __name__ == "__main__":
