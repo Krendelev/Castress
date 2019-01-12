e89a2f5fdd68381a4657d1f3826880b06b28afd3
m.semenov <m.semenov@corp.mail.ru>
2019-01-12 13:46:53 +0300

    мои комменты по коду

--- castress/audio_processing.py
+++ castress/audio_processing.py
@@ -12,16 +12,19 @@ def get_audio(text):
     }
     try:
         return requests.get(TTS_URL, params=payload, timeout=(1, 5)).content
+    # ???: лучше дать этому упасть наверное
     except (requests.exceptions.ConnectionError, requests.exceptions.ReadTimeout):
         return None
 
 
+# ???: а почему audio, если это массив из фаилов?
 def save_audio(audio, file_name):
     with open(file_name, "wb") as fh:
         for piece in audio:
             fh.write(piece)
 
 
+# TODO: Вынести в tests/audio_test.py
 if __name__ == "__main__":
     sample_text = [
         "Однажды, в студёную зимнюю пору",
@@ -30,4 +33,5 @@ if __name__ == "__main__":
         "Лошадка, везущая хворосту воз.",
     ]
     pieces = [get_audio(text) for text in sample_text]
+    print(pieces)
     save_audio(pieces, "test.mp3")
